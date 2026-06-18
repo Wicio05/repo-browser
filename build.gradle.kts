31811d75a1b417e5c73778c2b620746eafa33033
Witek Cybulski <witekcybul1@gmail.com>
2026-06-18 19:42:32 +0200

    chore: initial commit - project setup and configuration testing
    Addresses #1

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.1.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "pl.witekcybulski"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+
+	testImplementation("org.wiremock:wiremock-standalone:3.13.2")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+
+	jvmArgs(
+		"-XX:+EnableDynamicAgentLoading",
+		"-Xshare:off"
+	)
+}
